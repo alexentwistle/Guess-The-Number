a17b472d1c492beb7affa0428f7969146eadbcdf
Alex Entwistle <alexentwistleseo@gmail.com>
2019-03-19 12:43:49 +0000

    Added guessing again feature.

--- guessnumber.py
+++ guessnumber.py
@@ -2,19 +2,17 @@ import random
 
 result = random.randint(1,5)
 
-guess = int(input("Guess the number:"))
+guess = int(input("Hi. I'm thinking of a number between 1 and 5. Guess the number:"))
 
-
-while True:	
+while result != guess:
 	if guess < result:
-		print("Too low.")
-		break
+		print("Too low. Guess again?")
+		guess = int(input("Enter a number between 1 and 5:\n"))
 	elif guess > result: 
-		print("Too high.")
-		break
+		print("Too high. Guess again?")
+		guess = int(input("Enter a number between 1 and 5:\n"))
 	else: 
 		print("Well done!")
 		break
 
-
-# TODO - add guess again option
+print("Well done! The number was %d." % result)

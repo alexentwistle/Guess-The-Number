d9e1fe843ffd6e38c6718237a458d16062866026
Alex Entwistle <alexentwistleseo@gmail.com>
2019-03-19 12:29:41 +0000

    added file

---
+++ guessnumber.py
@@ -0,0 +1,17 @@
+import random
+
+result = random.randint(1,5)
+
+guess = int(input("Guess the number:"))
+
+
+while True:	
+	if guess < result:
+		print("Too low.")
+		break
+	elif guess > result: 
+		print("Too high.")
+		break
+	else: 
+		print("Well done!")
+		break

d344388fe68e79e02b913ab1319a0bb57dac2c9f
Alex Entwistle <alexentwistleseo@gmail.com>
2019-05-29 15:02:34 +0100

    Made the game a little harder

--- guessnumber.py
+++ guessnumber.py
@@ -1,17 +1,17 @@
 import random
 
-result = random.randint(1,5)
+result = random.randint(1,100)
 
-guess = int(input("Hi. I'm thinking of a number between 1 and 5. Guess the number:"))
+guess = int(input("Hi. I'm thinking of a number between 1 and 100. Guess the number:"))
 
 while result != guess:
 	if guess < result:
-		print("Too low. Guess again?")
-		guess = int(input("Enter a number between 1 and 5:\n"))
-	elif guess > result: 
-		print("Too high. Guess again?")
-		guess = int(input("Enter a number between 1 and 5:\n"))
+		print("Too low! Guess again?")
+		guess = int(input("Enter a number between 1 and 100:\n"))
+	elif guess > result:
+		print("Too high! Guess again?")
+		guess = int(input("Enter a number between 1 and 100:\n"))
 	else: 
 		break
 
-print("Well done! The number was %d." % result)
+print("Well done!!! The number was %d." % result)
